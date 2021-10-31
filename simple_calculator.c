a559b3b8849ebf2bb147b7b424a5db19430c377b
Egor Howell <77462366+egorhowell@users.noreply.github.com>
2021-10-31 19:55:59 +0000

    Add files via upload

---
+++ simple_calculator.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+
+	double num1;
+	double num2;
+	printf("");
+	printf("enter the first number");
+	scanf("%lf", &num1);
+	printf("");
+	printf("enter the second number");
+	scanf("%lf", &num2);
+	printf("");
+	printf("the answer is %f", num1+num2 );
+
+
+	return 0;
+
+
+
+}
