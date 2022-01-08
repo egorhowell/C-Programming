8c4a7ae5c7e34c3f85b53b7dce30f7fc7094dc33
Egor Howell <77462366+egorhowell@users.noreply.github.com>
2022-01-08 23:06:37 +0000

    Add files via upload

---
+++ recursion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+long int factorial(int n);
+
+int main() {
+
+    int n;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    printf("%ld", factorial(n));
+
+    return 0;
+}
+
+long int factorial(int n) {
+    if (n >= 1)
+        return n*factorial(n-1); 
+    else
+        return 1;
+}
